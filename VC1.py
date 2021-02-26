ad7538eeb3020e35cec5cff60a1de25b74ac3141
Student <student@PNCLB12-16.pnc.lan>
2021-02-26 10:45:50 +0700

    #3.wall

--- VC1.py
+++ VC1.py
@@ -1,26 +1,104 @@
-import tkinter as tk
-# Create an empty window
-root = tk.Tk()
-# Set TK window size to width 600 px and height 200 px
-root.geometry("550x600")
-# Create a frame in the window (frame is a container, like "div" in HTML)
-frame = tk.Frame()
-# Set the title of the frame
-frame.master.title("Hello PNC")
-canvas = tk.Canvas(frame) 
-def DrawGird():
-   for row in range(9):
-    for column in range(9):
-        oval=canvas.create_rectangle((row*50+50, column*50+50, 100+(row*50),100+(column*50)) ,fill="white")
-# Load the image
-myImage = tk.PhotoImage(file='.\player.png')
-# Add the image to the canvas
-myImageId = canvas.create_image(20, 20, image=myImage)
-# HERE YOU CAN START TO DRAW
-DrawGird()
-# pack means "draw what i put inside" 
-canvas.pack(expand=True, fill='both') 
-frame.pack(expand=True, fill='both')
+import tkinter as tk 
+#make empty window
+root=tk.Tk()
+# put screenwidth and 
+root.geometry("600x600")
+#veriable for stalled
+grid = [[1,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,2,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,2]]
+frame=tk.Frame()
+frame.master.title("Hey,Let Play Crazy Game")
+myImage=0
+myFlag=0
+def drawGrid():
+    global myImage,myFlag
+    for col in range(len(grid)):
+        for row in range(len(grid[0])):
+            if grid[col][row]==1:
+                # Load the image
+                myImage = tk.PhotoImage(file='.\player.png')
+                # Add the image to the canvas
+                myImageId = canvas.create_image(70,70,image=myImage)
+                # canvas.create_rectangle((row*50+50, col*50+50, 100+(row*50),100+(col*50)) ,fill="white")
+            else:
+                canvas.create_rectangle((row*50+50, col*50+50, 100+(row*50),100+(col*50)) ,fill="white")
 
-# Display all
+            if grid[9][9]==2:
+                #  Load the image
+                myFlag = tk.PhotoImage(file='.\flag.png')
+                # Add the image to the canvas
+                myImaged = canvas.create_image(500,500,image=myFlag)
+    canvas.create_rectangle(100,50,450,100,fill="green")   
+    canvas.create_rectangle(200,150,250,450,fill="green")   
+    canvas.create_rectangle(300,200,500,250,fill="green")   
+    canvas.create_rectangle(300,300,350,450,fill="green")   
+    canvas.create_rectangle(350,350,550,400,fill="green")   
+    canvas.create_rectangle(50,350,150,400,fill="green")   
+    canvas.create_rectangle(100,400,150,450,fill="green")   
+    canvas.create_rectangle(100,200,150,350,fill="green")  
+    canvas.create_rectangle(50,500,500,550,fill="green")  
+    return None
+def moveLeft(event):
+    global grid 
+    indexX=-1
+    indexY=-1
+    for col in range(len(grid)):
+        for row in range(len(grid[0])):
+            if grid[col][row]==1:
+                indexX=row
+                indexY=col
+    if  indexX>0:
+        grid[indexY][indexX]=0
+        grid[indexY][indexX-1]=1
+    drawGrid()
+    
+def moveRight(event):
+    global grid
+    indexX=-1
+    indexY=-1
+    for col in range(len(grid)):
+        for row in range(len(grid[0])):
+            if grid[col][row]==1:
+                indexX=row
+                indexY=col
+    if indexX<len(grid)-1:
+        grid[indexY][indexX]=0
+        grid[indexY][indexX+1]=1
+    drawGrid()
+def moveUp(event):
+    global grid
+    indexX=-1
+    indexY=-1
+    for col in range(len(grid)):
+        for row in range(len(grid[0])):
+           if grid[col][row]==1:
+               indexX=row
+               indexY=col
+    if  indexY>0:
+        grid[indexY][indexX]=0
+        grid[indexY-1][indexX]=1
+    drawGrid()
+def moveDown(event):
+    global grid
+    indexX=-1
+    indexY=-1
+    for  col in range(len(grid)):
+        for row in range(len(grid[0])):
+            if grid[col][row]==1:
+                indexX=row
+                indexY=col
+    if indexY<len(grid)-1:
+        grid[indexY][indexX]=0
+        grid[indexY+1][indexX]=1
+    drawGrid()
+canvas=tk.Canvas(root)
+root.bind("<Left>",moveLeft)#move to left
+root.bind("<Right>",moveRight)#move to right
+root.bind("<Up>",moveUp)#move to up
+root.bind("<Down>",moveDown)#move to down
+#move ball
+# canvas.after(1000,lambda:moveBall1(ball1))
+canvas.pack(expand=True,fill='both')
+drawGrid()
+root.resizable(False,False)
 root.mainloop()
+
