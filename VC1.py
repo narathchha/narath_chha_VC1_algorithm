c5c743773b0771f5e7f404a00a8b9376e718286d
Student <student@PNCLB12-16.pnc.lan>
2021-02-28 14:40:16 +0700

    #4.Anemies

--- VC1.py
+++ VC1.py
@@ -1,94 +1,129 @@
 import tkinter as tk 
+from tkinter import messagebox
 #make empty window
 root=tk.Tk()
 # put screenwidth and 
 root.geometry("600x600")
 #veriable for stalled
-grid = [[1,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,2,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,2]]
+grid = [
+[1,4,4,4,4,4,4,4,4,0,0],
+[0,2,2,2,2,2,2,2,2,0,0],
+[0,0,4,4,4,4,4,4,4,4,4],
+[0,0,2,2,2,2,2,2,2,0,4],
+[0,0,8,0,4,2,4,4,4,4,4],
+[0,0,0,0,4,2,0,0,0,0,0],
+[0,0,0,7,4,2,0,0,0,0,0],
+[0,0,0,5,4,2,0,0,6,0,0],
+[0,0,4,4,4,2,0,0,0,3,0],
+[0,0,2,2,2,2,2,2,2,0,0]]
 frame=tk.Frame()
-frame.master.title("Hey,Let Play Crazy Game")
-myImage=0
-myFlag=0
-def drawGrid():
-    global myImage,myFlag
-    for col in range(len(grid)):
-        for row in range(len(grid[0])):
-            if grid[col][row]==1:
-                # Load the image
-                myImage = tk.PhotoImage(file='.\player.png')
-                # Add the image to the canvas
-                myImageId = canvas.create_image(70,70,image=myImage)
-                # canvas.create_rectangle((row*50+50, col*50+50, 100+(row*50),100+(col*50)) ,fill="white")
-            else:
-                canvas.create_rectangle((row*50+50, col*50+50, 100+(row*50),100+(col*50)) ,fill="white")
+frame.master.title("Hey,Let's Play Crazy Game")
+myImage= tk.PhotoImage(file='player.png')
+myFlag = tk.PhotoImage(file='flags.png')
+myCoins = tk.PhotoImage(file='coinsilver.png')
+# myDiamond = tk.PhotoImage(file='diamond2.png')
+myAnemie1 = tk.PhotoImage(file='anemie1.png')
+myAnemie2 = tk.PhotoImage(file='anemies2.png')
+myAnemie3 = tk.PhotoImage(file='anemies3.png')
+# Display Background
+bg=tk.PhotoImage(file='background2.png')
+# canvas.create_image(0,0, image=bg,anchor="nw")
+# Sound
+# import Sound
+# winsound.PlaySound("error2.wav",winsound.SND_FILENAME)
+# Founction
 
-            if grid[9][9]==2:
-                #  Load the image
-                myFlag = tk.PhotoImage(file='.\flag.png')
-                # Add the image to the canvas
-                myImaged = canvas.create_image(500,500,image=myFlag)
-    canvas.create_rectangle(100,50,450,100,fill="green")   
-    canvas.create_rectangle(200,150,250,450,fill="green")   
-    canvas.create_rectangle(300,200,500,250,fill="green")   
-    canvas.create_rectangle(300,300,350,450,fill="green")   
-    canvas.create_rectangle(350,350,550,400,fill="green")   
-    canvas.create_rectangle(50,350,150,400,fill="green")   
-    canvas.create_rectangle(100,400,150,450,fill="green")   
-    canvas.create_rectangle(100,200,150,350,fill="green")  
-    canvas.create_rectangle(50,500,500,550,fill="green")  
-    return None
-def moveLeft(event):
-    global grid 
-    indexX=-1
-    indexY=-1
+
+
+def drawGrid():
+    global myImage,myFlag,myCoins,myDiamond,myAnemie1,myAnemies2,myAnemies3
+    canvas.delete("all")
+    canvas.create_image(50,50, image=bg)
+
     for col in range(len(grid)):
         for row in range(len(grid[0])):
             if grid[col][row]==1:
-                indexX=row
-                indexY=col
-    if  indexX>0:
-        grid[indexY][indexX]=0
-        grid[indexY][indexX-1]=1
+                # Add the image to the canvas
+                # myImageId = canvas.create_image(80,90,image=myImage)
+                canvas.create_image(row*50+70, col*50+70,image=myImage)
+            elif grid[col][row]==2:
+                canvas.create_rectangle((row*50+50, col*50+50, 100+(row*50),100+(col*50)) ,outline="green", fill="green")
+            elif grid[col][row]==3:
+                canvas.create_image(row*50+70, col*50+70,image=myFlag)
+            elif grid[col][row]==4:
+                canvas.create_image(row*50+70, col*50+70,image=myCoins)
+            # elif grid[col][row]==5:
+                # canvas.create_image(row*50+70, col*50+70,image=myDiamond)
+            elif grid[col][row]==6:
+                canvas.create_image(row*50+70, col*50+70,image=myAnemie1)
+            elif grid[col][row]==7:    
+                canvas.create_image(row*50+70, col*50+70,image=myAnemie2)
+            elif grid[col][row]==8:    
+                Move=canvas.create_image(row*50+70, col*50+70,image=myAnemie3)
+    return None
+def moveAnemie():
+    canvas.move(Move,2,2)
+def displayMessageWin():
+    messagebox.showinfo("Title", "You Win!")
+def moveLeft(event):
+
+    global grid 
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col]==1:
+                indexRow=row
+                indexCol=col
+    if indexCol>0:
+        if grid[indexRow][indexCol-1]==2:
+            displayMessageWin()
+        if grid[indexRow][indexCol-1]!=2:
+            grid[indexRow][indexCol]=0
+            grid[indexRow][indexCol-1]=1
     drawGrid()
     
 def moveRight(event):
     global grid
-    indexX=-1
-    indexY=-1
-    for col in range(len(grid)):
-        for row in range(len(grid[0])):
-            if grid[col][row]==1:
-                indexX=row
-                indexY=col
-    if indexX<len(grid)-1:
-        grid[indexY][indexX]=0
-        grid[indexY][indexX+1]=1
+    # indexX=-1
+    # indexY=-1
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col]==1:
+                indexRow=row
+                indexCol=col
+    if indexCol <len(grid[0])-1:
+        if grid[indexRow][indexCol+1]==3:
+            displayMessageWin()
+        if grid[indexRow][indexCol+1]!=2:
+            grid[indexRow][indexCol]=0
+            grid[indexRow][indexCol+1]=1
     drawGrid()
 def moveUp(event):
     global grid
-    indexX=-1
-    indexY=-1
-    for col in range(len(grid)):
-        for row in range(len(grid[0])):
-           if grid[col][row]==1:
-               indexX=row
-               indexY=col
-    if  indexY>0:
-        grid[indexY][indexX]=0
-        grid[indexY-1][indexX]=1
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col]==1:
+                indexRow=row
+                indexCol=col
+    if indexRow >0:
+        if grid[indexRow-1][indexCol]==3:
+            displayMessageWin()
+        if grid[indexRow-1][indexCol]!=2:
+            grid[indexRow][indexCol]=0
+            grid[indexRow-1][indexCol]=1
     drawGrid()
 def moveDown(event):
     global grid
-    indexX=-1
-    indexY=-1
-    for  col in range(len(grid)):
-        for row in range(len(grid[0])):
-            if grid[col][row]==1:
-                indexX=row
-                indexY=col
-    if indexY<len(grid)-1:
-        grid[indexY][indexX]=0
-        grid[indexY+1][indexX]=1
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col]==1:
+                indexRow=row
+                indexCol=col
+    if indexRow <len(grid)-1:
+        if grid[indexRow+1][indexCol]==3:
+            displayMessageWin()
+        if grid[indexRow+1][indexCol]!=2:
+            grid[indexRow][indexCol]=0
+            grid[indexRow+1][indexCol]=1
     drawGrid()
 canvas=tk.Canvas(root)
 root.bind("<Left>",moveLeft)#move to left
