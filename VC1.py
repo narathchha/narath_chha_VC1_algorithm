7f0879c554bd828693663e640e5937e9791224da
Student <student@PNCLB12-16.pnc.lan>
2021-03-03 10:29:38 +0700

    #7.finish

--- VC1.py
+++ VC1.py
@@ -12,10 +12,10 @@ grid = [
 [0,2,2,2,2,2,2,2,2,0,0],
 [0,0,4,4,4,4,4,4,4,4,4],
 [0,0,2,2,2,2,2,2,2,0,4],
-[0,0,8,0,4,2,4,4,4,4,4],
-[0,0,0,0,4,2,0,0,0,6,0],
+[0,0,7,0,4,2,4,4,4,4,4],
+[0,0,0,0,4,2,0,0,7,6,0],
 [0,0,0,6,4,2,0,0,0,0,0],
-[0,0,0,5,4,2,0,0,6,0,0],
+[0,7,0,5,4,2,0,0,6,0,0],  
 [0,0,4,4,4,2,0,0,0,3,0],
 [0,0,2,2,2,2,2,2,2,0,0]]
 frame=tk.Frame()
@@ -26,17 +26,11 @@ myFlag = tk.PhotoImage(file='flags.png')
 myCoins = tk.PhotoImage(file='coinsilver.png')
 # myDiamond = tk.PhotoImage(file='diamond2.png')
 myAnemie1 = tk.PhotoImage(file='anemie1.png')
+myDiamond = tk.PhotoImage(file='diamond.png')
 myGameover = tk.PhotoImage(file='gameover.png')
 myWin = tk.PhotoImage(file='youwin.png')
 # Display Background
 bg=tk.PhotoImage(file='background2.png')
-# canvas.create_image(0,0, image=bg,anchor="nw")
-# Sound
-# import Sound
-# winsound.PlaySound("error2.wav",winsound.SND_FILENAME)
-# Founction
-
-
 
 def drawGrid():
     global myImage,myFlag,myCoins,myDiamond,myAnemie1,score
@@ -45,8 +39,6 @@ def drawGrid():
     for col in range(len(grid)):
         for row in range(len(grid[0])):
             if grid[col][row]==1:
-                # Add the image to the canvas
-                # myImageId = canvas.create_image(80,90,image=myImage)
                 canvas.create_image(row*50+70, col*50+70,image=myImage)
             elif grid[col][row]==2:
                 canvas.create_rectangle((row*50+50, col*50+50, 100+(row*50),100+(col*50)) ,outline="lightblue", fill="lightblue")
@@ -54,19 +46,26 @@ def drawGrid():
                 canvas.create_image(row*50+70, col*50+70,image=myFlag)
             elif grid[col][row]==4:
                 canvas.create_image(row*50+70, col*50+70,image=myCoins)
-            # elif grid[col][row]==5:
-                # canvas.create_image(row*50+70, col*50+70,image=myDiamond)
             elif grid[col][row]==6:
                 canvas.create_image(row*50+70, col*50+70,image=myAnemie1)
+            elif grid[col][row]==7:
+                canvas.create_image(row*50+70, col*50+70,image=myDiamond)
+    canvas.create_text(100,20, text="Score="+str(score),font=("Purisa",30),fill="lightblue")
+    canvas.create_text(400,20, text="Time="+str(time),font=("Purisa",30),fill="lightblue")
     return None
-def moveAnemie():
-    canvas.move(Move,2,2)
+time=60
+def reduceTime():
+    global time
+    time=time-1
+    drawGrid()
+    # canvas.after(1000,reduceTime())
 def displayMessageWin():
     messagebox.showinfo("Title", "You Win!")
+    root.destroy()
 def displayMessageLost():
     messagebox.showinfo("Title", "You Lost!")
+    root.destroy()
 def moveLeft(event):
-
     global grid ,score,myGameover,myWin
     for row in range(len(grid)):
         for col in range(len(grid[0])):
@@ -75,22 +74,23 @@ def moveLeft(event):
                 indexCol=col
     if indexCol>0:
         if grid[indexRow][indexCol-1]==3:
-            winsound .PlaySound("win.wav", winsound.SND_FILENAME)
-            canvas.create_image(400,50,image=myWin)
+            winsound .PlaySound("win.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
+            canvas.create_image(320,300,image=myWin)
             displayMessageWin()
         if grid[indexRow][indexCol-1]==4:
             #  Play the sound 
-            winsound .PlaySound("coin5.wav", winsound.SND_FILENAME)
+            winsound .PlaySound("coin5.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
             score+=20
         if grid[indexRow][indexCol-1]==6:
-            winsound .PlaySound("hit3.wav", winsound.SND_FILENAME)
-            canvas.create_image(400,50,image=myGameover)
+            winsound .PlaySound("hit3.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
+            canvas.create_image(320,300,image=myGameover)
             displayMessageLost()
+        if grid[indexRow][indexCol-1]==7:
+            score+=60
         if grid[indexRow][indexCol-1]!=2:
             grid[indexRow][indexCol]=0
             grid[indexRow][indexCol-1]=1
     drawGrid()
-    # canvas.create_text(100,100,fill="blue")
 def moveRight(event):
     global grid,score,myGameover,myWin
     # indexX=-1
@@ -102,17 +102,19 @@ def moveRight(event):
                 indexCol=col
     if indexCol <len(grid[0])-1:
         if grid[indexRow][indexCol+1]==3:
-            winsound .PlaySound("win.wav", winsound.SND_FILENAME)
-            canvas.create_image(400,50,image=myWin)
+            winsound .PlaySound("win.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
+            canvas.create_image(320,300,image=myWin)
             displayMessageWin()
         if grid[indexRow][indexCol+1]==4:
             #  Play the sound 
-            winsound .PlaySound("coin5.wav", winsound.SND_FILENAME)
+            winsound .PlaySound("coin5.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
             score+=20
         if grid[indexRow][indexCol+1]==6:
-            winsound .PlaySound("hit3.wav", winsound.SND_FILENAME)
-            canvas.create_image(400,50,image=myGameover)
+            winsound .PlaySound("hit3.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
+            canvas.create_image(320,300,image=myGameover)
             displayMessageLost()
+        if grid[indexRow][indexCol+1]==7:
+            score+=60
         if grid[indexRow][indexCol+1]!=2:
             grid[indexRow][indexCol]=0
             grid[indexRow][indexCol+1]=1
@@ -126,22 +128,23 @@ def moveUp(event):
                 indexCol=col
     if indexRow >0:
         if grid[indexRow-1][indexCol]==3:
-            winsound .PlaySound("win.wav", winsound.SND_FILENAME)
-            canvas.create_image(400,50,image=myWin)
+            winsound .PlaySound("win.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
+            canvas.create_image(320,300,image=myWin)
             displayMessageWin()
         if grid[indexRow-1][indexCol]==4:
             # Play the sound 
-            winsound .PlaySound("coin5.wav", winsound.SND_FILENAME)
+            winsound .PlaySound("coin5.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
             score+=20
         if grid[indexRow-1][indexCol]==6:
-            winsound .PlaySound("hit3.wav", winsound.SND_FILENAME)
-            canvas.create_image(400,50,image=myGameover)
+            winsound .PlaySound("hit3.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
+            canvas.create_image(320,300,image=myGameover)
             displayMessageLost()
+        if grid[indexRow-1][indexCol]==7:
+            score+=60
         if grid[indexRow-1][indexCol]!=2:
             grid[indexRow][indexCol]=0
             grid[indexRow-1][indexCol]=1
     drawGrid()
-    # canvas.create_text(100,100,fill="blue")
 def moveDown(event):
     global grid,score,myGameover,myWin
     for row in range(len(grid)):
@@ -151,30 +154,29 @@ def moveDown(event):
                 indexCol=col
     if indexRow <len(grid)-1:
         if grid[indexRow+1][indexCol]==3:
-            winsound .PlaySound("win.wav", winsound.SND_FILENAME)
-            canvas.create_image(400,50,image=myWin)
+            winsound.PlaySound("win.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
+            canvas.create_image(320,300,image=myWin)
             displayMessageWin()
         if grid[indexRow+1][indexCol]==4:
             # Play the sound 
-            winsound .PlaySound("coin5.wav", winsound.SND_FILENAME)
+            winsound .PlaySound("coin5.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
             score+=20
-            if grid[indexRow+1][indexCol]==6:
-                winsound .PlaySound("hit3.wav", winsound.SND_FILENAME)
-                canvas.create_image(400,50,image=myGameover)
-                displayMessageLost()
+        if grid[indexRow+1][indexCol]==6:
+            winsound .PlaySound("hit3.wav", winsound.SND_FILENAME+winsound.SND_ASYNC)
+            canvas.create_image(320,300,image=myGameover)
+            displayMessageLost()
+        if grid[indexRow+1][indexCol]==7:
+            score+=60
         if grid[indexRow+1][indexCol]!=2:
             grid[indexRow][indexCol]=0
             grid[indexRow+1][indexCol]=1
     drawGrid()
-    # canvas.create_text(100,100,fill="blue")
-
 canvas=tk.Canvas(root)
+canvas.after(10,reduceTime())
 root.bind("<Left>",moveLeft)#move to left
 root.bind("<Right>",moveRight)#move to right
 root.bind("<Up>",moveUp)#move to up
 root.bind("<Down>",moveDown)#move to down
-#move ball
-# canvas.after(1000,lambda:moveBall1(ball1))
 canvas.pack(expand=True,fill='both')
 drawGrid()
 root.resizable(False,False)
